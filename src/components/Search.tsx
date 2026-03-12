6b049721b38aa7eb397dda369c0e912af625b31f
Roman <vikulov.roman.work@gmail.com>
2026-03-12 21:10:02 +0200

    Update Search.tsx

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,3 +1,5 @@
+import searchIcon from "../assets/search.svg"
+
 interface Props {
   value: string
   onChange: (value: string) => void
@@ -5,11 +7,19 @@ interface Props {
 
 export default function Search({ value, onChange }: Props) {
   return (
-    <input
-      className="header-search"
-      placeholder="Поиск"
-      value={value}
-      onChange={(e) => onChange(e.target.value)}
-    />
+    <div className="search-box">
+      <img
+        src={searchIcon}
+        className="search-icon"
+        alt="search"
+      />
+
+      <input
+        className="header-search"
+        placeholder="Поиск"
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+      />
+    </div>
   )
 }
