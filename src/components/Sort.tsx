9c1e1c5662eb5650d8acc96f1425a551b1a4fbff
Roman <vikulov.roman.work@gmail.com>
2026-03-12 19:53:43 +0200

    Update Sort.tsx

--- src/components/Sort.tsx
+++ src/components/Sort.tsx
@@ -1,13 +1,87 @@
+import { useState } from "react"
+import vectorUp from "../assets/vectorUp.svg"
+import vectorDown from "../assets/vectorDown.svg"
+
+
 interface Props {
+  total: number
   value: string
-  onChange: (value: string) => void
+  direction: "asc" | "desc"
+  onChange: (value: string, direction: "asc" | "desc") => void
 }
 
-export default function Sort({ value, onChange }: Props) {
+export default function Sort({ total, value, direction, onChange }: Props) {
+
+  const [open, setOpen] = useState(false)
+
+  const toggleDirection = () => {
+    const newDir = direction === "asc" ? "desc" : "asc"
+    onChange(value, newDir)
+  }
+
+  const changeSort = (type: string) => {
+  onChange(type, "desc")
+  setOpen(false)
+}
+
+  const label =
+    value === "price"
+      ? "По цене"
+      : "По популярности"
+
   return (
-    <select value={value} onChange={(e) => onChange(e.target.value)}>
-      <option value="price">Цена ↑</option>
-      <option value="rating">Рейтинг</option>
-    </select>
+    <div className="catalog-toolbar">
+
+      <div className="catalog-count">
+        {total} товаров
+      </div>
+
+      <div className="sort">
+
+        <div className="sort-direction" onClick={toggleDirection}>
+
+          <img
+            src={vectorUp}
+            className={`sort-arrow-icon ${direction === "asc" ? "active" : ""
+              }`}
+          />
+
+          <img
+            src={vectorDown}
+            className={`sort-arrow-icon ${direction === "desc" ? "active" : ""
+              }`}
+          />
+
+        </div>
+
+        <div className="sort-select">
+
+          <div
+            className="sort-current"
+            onClick={() => setOpen(!open)}
+          >
+            {label}
+            <span className="sort-arrow">▼</span>
+          </div>
+
+          {open && (
+            <div className="sort-dropdown">
+
+              <div onClick={() => changeSort("rating")}>
+                По популярности
+              </div>
+
+              <div onClick={() => changeSort("price")}>
+                По цене
+              </div>
+
+            </div>
+          )}
+
+        </div>
+
+      </div>
+
+    </div>
   )
 }
