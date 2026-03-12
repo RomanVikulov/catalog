1774a44185003c1b5f6c214c1b28ae8de8a9fa07
Roman <vikulov.roman.work@gmail.com>
2026-03-12 19:54:02 +0200

    Update api.ts

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,12 +1,11 @@
 import type { Product } from "../types/ProductType"
-
-const API_URL =
-  "https://ip-194-99-21-145-139178.vps.hosted-by-mvps.net/api/v1/products"
-
-
 export async function fetchProducts(): Promise<Product[]> {
-  const res = await fetch(API_URL)
-  const json = await res.json()
+  const res = await fetch("/api/v1/products")
 
+  if (!res.ok) {
+    throw new Error("API error")
+  }
+
+  const json = await res.json()
   return json.data.products
 }
