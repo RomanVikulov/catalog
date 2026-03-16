d2eadb77d19418fa207a5ee3749e15b6ae3261e0
Roman <vikulov.roman.work@gmail.com>
2026-03-16 20:39:49 +0200

    улучшил решение и добавил live-деплой.

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,11 @@
 import type { Product } from "../types/ProductType"
+
+const API_URL = import.meta.env.DEV
+  ? "/api/v1/products"
+  : "https://ip-194-99-21-145-139178.vps.hosted-by-mvps.net/api/v1/products"
+
 export async function fetchProducts(): Promise<Product[]> {
-  const res = await fetch("/api/v1/products")
+  const res = await fetch(API_URL)
 
   if (!res.ok) {
     throw new Error("API error")
